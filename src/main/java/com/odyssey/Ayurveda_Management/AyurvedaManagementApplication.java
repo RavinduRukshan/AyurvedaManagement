48e79e2bba7dc1796c96bd5bc84c0df004849954
RavinduRukshan <ravindurukshan1015@gmail.com>
2025-01-09 20:47:14 +0530

    Ayurveda Management Initializer

---
+++ src/main/java/com/odyssey/Ayurveda_Management/AyurvedaManagementApplication.java
@@ -0,0 +1,13 @@
+package com.odyssey.Ayurveda_Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyurvedaManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AyurvedaManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/odyssey/Ayurveda_Management/AyurvedaManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odyssey.Ayurveda_Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AyurvedaManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
